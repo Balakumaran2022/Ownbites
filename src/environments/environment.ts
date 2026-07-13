b04076816c6fa97a6a41bd1b5ac551c994af038e
BALAKUMARAN <balakumarancse2022@gmail.com>
2026-07-13 10:03:10 +0530

    feat: delete netlify.toml, create root .env configuration, and configure absolute apiUrl in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://backend2.owct.me',
   belongsTo: '69b256ec99b143835b75ee69',
   outletId: '6a26675eee35a470359a1c44'
 };
