37759cef2ed0859086e83da80abc794ea90d9aeb
BALAKUMARAN <balakumarancse2022@gmail.com>
2026-07-13 10:51:17 +0530

    fix: update all proxy and env backend configurations to target active backend3.owct.me server

--- react-app/src/environment.ts
+++ react-app/src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend2.owct.me',
+  apiUrl: 'https://backend3.owct.me',
   belongsTo: '69b256ec99b143835b75ee69',
   outletId: '6a26675eee35a470359a1c44'
 };
