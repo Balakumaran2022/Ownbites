d9f70ff6cfb8f6ba37ae4164264a6c77b4ceb717
BALAKUMARAN <balakumarancse2022@gmail.com>
2026-07-13 10:28:27 +0530

    fix: implement dynamic runtime localhost origin detection for environment apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend2.owct.me',
+  apiUrl: (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')) ? '' : 'https://backend2.owct.me',
   belongsTo: '69b256ec99b143835b75ee69',
   outletId: '6a26675eee35a470359a1c44'
 };
